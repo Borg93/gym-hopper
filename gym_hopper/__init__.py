313f4fd9d13bfd71b066f0fda008c9aa00844202
HeinzBenjamin <bfelbrich@hotmail.com>
2016-10-27 16:39:14 +0200

    split btw plane control and six axis control

--- gym_hopper/__init__.py
+++ gym_hopper/__init__.py
@@ -2,6 +2,12 @@ from gym.envs.registration import register
 
 
 register(
-    id="hopper-v0",
-    entry_point="gym_hopper.envs:GymHopper",
-    timestep_limit=1000)
+    id="hopper-sac-v0",
+    entry_point="gym_hopper.envs:GymHopperSAC",
+    timestep_limit=10000)
+
+register(
+    id="hopper-pc-v0",
+    entry_point="gym_hopper.envs:GymHopperPC",
+    timestep_limit=10000)
+
